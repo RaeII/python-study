9c61d08b4444300568d5b36526347a967359e898
Israel Zeferino <israel.zeferino@hotmail.com>
2023-11-15 22:27:27 -0300

    variaveis

---
+++ 1.variaveis/index.py
@@ -0,0 +1,82 @@
+print('\n ========= INT ========= \n\n')
+
+idade = 20
+ano = 2010
+
+print(type(idade))
+print(type(ano))
+
+print('\n\n ========= INT ========= \n')
+
+print('\n ========= FLOAT ========= \n\n')
+
+altura = 1.73
+peso = 78.500
+
+print(type(peso))
+print(type(altura))
+
+print('\n\n ========= FLOAT ========= \n')
+
+print('\n ========= COMPLEX ========= \n\n')
+
+a = 10+16j
+b = 3+80j
+
+print(type(a))
+print(type(b))
+
+print('\n\n ========= COMPLEX ========= \n')
+
+print('\n ========= STRING ========= \n\n')
+
+nome = 'Guilherme'
+profissao = 'Engenheiro de Software'
+
+print(type(profissao ))
+print(type(nome))
+
+print('\n\n ========= STRING ========= \n')
+
+print('\n ========= BOOL ========= \n\n')
+
+sexta_feira = True
+feriado = False
+
+print(type(sexta_feira))
+print(type(feriado))
+
+print('\n\n ========= BOOL ========= \n')
+
+print('\n ========= LIST ========= \n\n')
+
+alunos = ['Mônica', 'Ana', 'Bruno', 'Alice']
+notas = [10, 8.5, 7.8, 8.0]
+
+print(type(alunos))
+print(type(notas))
+
+print('\n\n ========= LIST ========= \n')
+
+#Tuple seu valores são imutaveis, são declarados com "()" e tem sua utilização igual List
+print('\n ========= TUPLE ========= \n\n')
+
+valores = (80, 29, 45, 91, 23)
+pontos = (10, 29.05, 66.8, 72)
+
+print(type(valores))
+print(type(pontos))
+
+print('\n\n ========= TUPLE ========= \n')
+
+print('\n ========= Dicionários (DICT) ========= \n\n')
+
+altura={"Sandra":1.65, "Elizabeth": 1.60, "Roberto": 1.70}
+idade = {'Sandra':35, "Elizabeth":58, "Roberto":68}
+
+print(altura['Sandra'])
+print(type(altura))
+print(type(idade))
+
+print('\n\n ========= Dicionários (DICT) ========= \n')
+
